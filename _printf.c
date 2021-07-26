de6fb223a5f305e437fcac3b734958b9c439c688
adute <aduti.alemu@gmail.com>
2021-07-26 10:58:37 -0700

    2nd commit

---
+++ _printf.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * _printf - function that prints 
+ * @format: type of variable
+ *
+ * Return: int
+ */
+int _printf(const char *format, ...)
+{
+
+str_print(format);
+}
