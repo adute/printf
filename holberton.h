ead3a856de63d1418e0e92a24c4d11c8f91d5341
Light-name <tosinemmaakin@gmail.com>
2021-07-26 12:28:02 -0700

    header file

--- holberton.h
+++ holberton.h
@@ -4,4 +4,13 @@
 
 str_print(const char *format, ...);
 _printf(const char *format, ...);
+
+/* */
+int print_unsignedIntToHex(unsigned int num, char _case);
+int print_char(va_list arg);
+int print_int(va_list arg);
+int print_STR(va_list arg);
+int print_str(va_list arg);
+int print_unsigned(va_list arg);
+
 #endif /* HOLBERTON_H*/
