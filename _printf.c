b24153fa1dd5e61ed5eedd25cd132e1a3615b826
adute <aduti.alemu@gmail.com>
2021-07-27 06:33:51 -0700

    Print

--- _printf.c
+++ _printf.c
@@ -7,6 +7,26 @@
  */
 int _printf(const char *format, ...)
 {
+	char *str;
+	va_list arg; 
+	int r;
 
-str_print(format);
+	va_start(arg);
+	 for (str = format; *str != '\0'; str++)
+	 {
+		 while (*str != '%')
+		 {
+			i =  write (1, *str, 1);
+			 str++;
+		 }
+		 str++;
+		 switch (*str)
+		 {
+			 case 's':
+				 s = va_arg(arg, char *);
+				i = write(1, format, sizeof(s));
+		 }
+	 }///str = va_arg(arg, char *);
+			va_end(arg);
+	return (i);
 }
